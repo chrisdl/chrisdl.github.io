541b32cc372490e247bb9baab934c5b09e16ff09
chris dilorenzo <dilorenzo.christopher@gmail.com>
2016-03-25 00:30:19 -0400

    adding npm and babel, node-sass and browser-sync, because clearly this project requires all the bells and whistles! also add normalize

---
+++ assets/js/main.es6.js
@@ -0,0 +1,3 @@
+console.log(
+  `I don't think I need any scripts to run for a page this simple. Do you?`
+);
---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+"use strict";
+
+console.log("I don't think I need any scripts to run for a page this simple. Do you?");
